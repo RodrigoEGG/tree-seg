95e9be57b7c57dea97e8176e524e13c0d10dbac3
rod <rodrigo.eguiluz@udem.edu>
2025-02-11 14:34:38 -0600

    Sprint 1: Only visual aspect of website

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import { Button } from "@/components/ui/button"
+import UploadData from './uploadData';
+
+const UploadDataModal: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>
+        Upload Data
+      </Button>
+      <Modal title="Upload your Files" 
+      open={isModalOpen} 
+      onCancel={handleCancel}
+      footer={false}>
+        <UploadData />
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
+          <Button onClick={handleOk}>Upload</Button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default UploadDataModal;
