b1a397229f1ec5018058ec5103706c0ace4bf215
norma <norma.elizondo@udem.edu>
2025-03-18 20:27:46 -0600

    First iteration on projects

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -3,6 +3,7 @@ import Landing from "./views/landing/Landing";
 import MainPage from "./layouts/MainPage";
 import Auth from "./layouts/Auth";
 import Login from "./views/login/Login";
+import Projects from "./views/projects/Projects";
 import Visualization from "./views/visualization/Visualization";
 
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
 			{
 				path : "/visualization",
 				element : <Visualization/>
+			},
+			{
+				path : "/projects",
+				element : <Projects/>
 			}
 		]
 
---
+++ app/src/views/projects/Projects.tsx
@@ -0,0 +1,16 @@
+import { ProjectTable } from "@/components/projects-table";
+export default function Projects() {
+
+    return (
+        
+        <>
+
+            <div className="flex-1 ml-8 mr-8">
+
+                <ProjectTable />
+
+            </div>
+
+        </>
+    )
+};
