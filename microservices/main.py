415a3c65ddf493e98ade2b33422aa3f034537035
norma <norma.elizondo@udem.edu>
2025-03-18 18:51:46 -0600

    DBs Config modifications

--- microservices/main.py
+++ microservices/main.py
@@ -1,16 +1,22 @@
 import uvicorn
 import os
 from fastapi import FastAPI
-from app.config.db_config import connect_to_db
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import api_router
 from pymongo import MongoClient
+from sqlalchemy.orm import Session
 from dotenv import dotenv_values
+from microservices.app.config.db_config_mongo import connect_to_db_mongo
+from microservices.app.config.db_config_postgres import connect_to_db_postgres, SessionLocal
+
 # Initialize FastAPI app
 app = FastAPI()
 
 # Connect to MongoDB at startup
-db_client = connect_to_db()  
+db_client_mongo = connect_to_db_mongo()  
+
+# Connect to PostgreSQL at startup
+db_client_postgres = connect_to_db_postgres()
 
 # load environment variables
 CONFIG = dotenv_values(".env")
@@ -24,14 +30,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Startup event
 @app.on_event("startup")
-def startup_db_client():
+def startup_db_client_():
+    # MongoDB
     app.mongodb_client = MongoClient(CONFIG["ATLAS_URI"])
     app.database = app.mongodb_client[CONFIG["ATLAS_DB_NAME"]]
+    print("Connected to MongoDB!")
 
+    # PostgreSQL
+    app.postgres_db = SessionLocal()
+    print("Connected to PostgreSQL!")
+
+
+# Shutdown event
 @app.on_event("shutdown")
 def shutdown_db_client():
+    # MongoDB
     app.mongodb_client.close()
+
+    # PostgreSQL
+    app.postgres_db.close()
+
 
 @app.get("/health")
 async def health():
