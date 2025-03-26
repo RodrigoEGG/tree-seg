8a146ea4c7a0b3f28d5ebafb09824d7ca484ed7c
norma <norma.elizondo@udem.edu>
2025-03-25 18:20:01 -0600

    Updated projects view

--- app/src/views/projects/Projects.tsx
+++ app/src/views/projects/Projects.tsx
@@ -1,16 +1,18 @@
 import { ProjectTable } from "@/components/projects-table";
+import { AppSidebar } from "@/components/app-sidebar";
+
 export default function Projects() {
 
     return (
-        
-        <>
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <AppSidebar />
 
-            <div className="flex-1 ml-8 mr-8">
-
-                <ProjectTable />
-
+            <div className="flex bg-gray-100 flex-col">
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                    <ProjectTable />
+                </main>
             </div>
 
-        </>
+        </div>
     )
-};
+}
