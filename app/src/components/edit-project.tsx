4adcd9019f3cc06a5e46a7e7e7aa0922d7bf3ea8
norma <norma.elizondo@udem.edu>
2025-04-04 18:39:40 -0600

    Edit projects modal

--- app/src/components/edit-project.tsx
+++ app/src/components/edit-project.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Modal } from 'antd';
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const EditProjectModal: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -32,13 +35,35 @@ const EditProjectModal: React.FC = () => {
                 open={isModalOpen}
                 onCancel={handleCancel}
                 footer={false}>
-                
+
+                <form className='flex flex-col gap-6'>
+                    <fieldset>
+                        <div className="form-group space-y-2">
+                            <Label htmlFor="project-name">Project Name:</Label>
+                            <Input type="text" id="project-name" placeholder="Name" />
+                        </div>
+                        <div className="form-group space-y-2">
+                            <Label htmlFor="project-description">Project Description:</Label>
+                            <Textarea id="project-description" placeholder="Description" rows={4} />
+                        </div>
+                        <div className="form-group space-y-2">
+                            <Label htmlFor="project-owner">Project Owner:</Label>
+                            <Textarea id="project-owner" placeholder="Owner" />
+                        </div>
+                        <div className="form-group space-y-2">
+                            <Label htmlFor="project-date">Project Date:</Label>
+                            <Input type="date" id="project-date" placeholder="Date" />
+                        </div>
+                    </fieldset>
+
+
                 <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
                     <Button variant="secondary" onClick={handleDelete} style={{ marginRight: '1rem' }}>
                         Delete Project
                     </Button>
                     <Button onClick={handleOk}>Make Changes</Button>
                 </div>
+            </form>
             </Modal>
         </>
     );
