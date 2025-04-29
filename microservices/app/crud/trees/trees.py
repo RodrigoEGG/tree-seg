4879adb838912473c0cd0c7306a1f0757766458b
rod <rodrigo.eguiluz@udem.edu>
2025-04-28 18:52:02 -0600

    Finished Diameter and Height calculation

--- microservices/app/crud/trees/trees.py
+++ microservices/app/crud/trees/trees.py
@@ -1,3 +1,6 @@
+from scipy.spatial.distance import pdist
+from scipy.spatial import ConvexHull
+import numpy as np
 
 def get_tree_heights(las) -> list:
     """
@@ -9,23 +12,28 @@ def get_tree_heights(las) -> list:
 
         segment_ids = las['PredInstance'].unique()
 
+        # Get the ground points
+        ground_points = las[las['Classification'] == 2]
+        ground_elevation = np.mean(ground_z)
+
+
         for segment in segment_ids:
             if segment == 0:
                 continue
             # Get the points for the current segment
             points = las[las['PredInstance'] == segment]
 
-            # Calculate the height of the tree
-            min_z = points['Z'].min()
-            max_z = points['Z'].max()
-            height = max_z - min_z
+            # Find the maximum Z (highest point)
+            max_z = np.max(z)
+
+            # Calculate tree height
+            tree_height = max_z - ground_elevation
 
             # Append the height to the list
-            heights.append(height)
+            heights.append(tree_height)
         return heights
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
-    return heights
 
 def get_tree_diameter(las) -> list:
     """
@@ -36,11 +44,36 @@ def get_tree_diameter(las) -> list:
         diameters = []
 
         segment_ids = las['PredInstance'].unique()
+        threshold = 2.0  # meters
 
         for segment in segment_ids:
             if segment == 0:
                 continue
-            # LOGIC FOR DIAMETER CALCULATION
+
+            # Get the points for the current segment
+            points = las[las['PredInstance'] == segment]
+
+            z = points.z
+            z_max = np.max(z)
+
+            # Get points near the top (within 2m below the top)
+            top_points_mask = (z >= (z_max - threshold))
+            x_top = points.x[top_points_mask]
+            y_top = points.y[top_points_mask]
+
+            # Stack them
+            xy_top = np.vstack((x_top, y_top)).T
+
+            # Use convex hull to get the outer boundary
+            hull = ConvexHull(xy_top)
+
+            # Get hull vertices
+            hull_points = xy_top[hull.vertices]
+
+            dists = pdist(hull_points)
+            crown_diameter = np.max(dists)
+
+            diameters.append(crown_diameter)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
     return diameters
