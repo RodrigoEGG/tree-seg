e06561461329d7b6329818e843d40c0d3dac16b9
Juan Cuevas <juan.m.cuevas@outlook.com>
2025-05-02 11:12:50 -0600

    Tree viewer test

--- app/src/context/TreeProvider.tsx
+++ app/src/context/TreeProvider.tsx
@@ -1,53 +1,24 @@
 // @ts-nocheck
-import React, { createContext, useContext, useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import React, { createContext, useContext, useRef, useState } from "react";
 
 const TreeContext = createContext<ViewerContextType | undefined>(undefined);
 
 export const TreeProvider: React.FC = ({ children }) => {
-
-	const { projectid ,  fileid , treeid } = useParams();
-
 	const potreeContainerRef = useRef<HTMLDivElement | null>(null);
+
 	const [viewer, setViewer] = useState<any>(null);
-	const [markers, setMarkers] = useState<any>([]);
+	const [markers, setMarkers] = useState<any[]>([]);
 
-	useEffect(() => {
-
-		if (!window.Potree) {
-			console.error("Potree library is not loaded!");
-			return;
-		}
-
-		if (potreeContainerRef.current) {
-			const newViewer = new window.Potree.Viewer(potreeContainerRef.current);
-			newViewer.setEDLEnabled(true);
-			newViewer.setFOV(60	);
-			newViewer.setPointBudget(10000 * 1000);
-			newViewer.setClipTask(window.Potree.ClipTask.SHOW_INSIDE);
-			newViewer.loadSettingsFromURL();
-			newViewer.setControls(newViewer.orbitControls);
-			setViewer(newViewer);
-
-			const bucket = import.meta.env.VITE_BUCKET_URL;
-			const url = `${bucket}/${projectid}/${fileid}/potree/metadata.json`
-			window.Potree.loadPointCloud(url)
-				.then((e: any) => {
-				const pointcloud = e.pointcloud;
-				const material = pointcloud.material;
-				material.activeAttributeName = "point source id";
-				material.minSize = 2;
-				material.pointSizeType = window.Potree.PointSizeType.FIXED;
-				newViewer.scene.addPointCloud(pointcloud);
-				newViewer.setFilterPointSourceIDRange(treeid,treeid);
-				newViewer.fitToScreen();
-				})
-				.catch((error: any) => console.error("ERROR: ", error));
-		}
-	}, []);
+	const contextValue = {
+		viewer,
+		setViewer,
+		potreeContainerRef,
+		markers,
+		setMarkers,
+	};
 
 	return (
-		<TreeContext.Provider value={{ viewer, setViewer, potreeContainerRef , markers, setMarkers}}>
+		<TreeContext.Provider value={contextValue}>
 			{children}
 		</TreeContext.Provider>
 	);
@@ -55,5 +26,8 @@ export const TreeProvider: React.FC = ({ children }) => {
 
 export const useTree = (): ViewerContextType => {
 	const context = useContext(TreeContext);
+	if (!context) {
+		throw new Error("useTree debe usarse dentro de un TreeProvider");
+	}
 	return context;
 };
--- app/src/views/tree/components/Viewer.tsx
+++ app/src/views/tree/components/Viewer.tsx
@@ -1,25 +1,61 @@
 // @ts-nocheck
+// Viewer.tsx
 import { useTree } from "@/context/TreeProvider";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Viewer() {
 	const { viewer, setViewer, potreeContainerRef } = useTree();
-  
+	const { projectid, fileid, treeid } = useParams();
+
+	useEffect(() => {
+		if (!window.Potree) {
+			console.error("Potree library is not loaded!");
+			return;
+		}
+
+		if (potreeContainerRef.current && !viewer) {
+			const newViewer = new window.Potree.Viewer(potreeContainerRef.current);
+			newViewer.setEDLEnabled(true);
+			newViewer.setFOV(60);
+			newViewer.setPointBudget(10000 * 1000);
+			newViewer.setClipTask(window.Potree.ClipTask.SHOW_INSIDE);
+			newViewer.loadSettingsFromURL();
+			newViewer.setControls(newViewer.orbitControls);
+			setViewer(newViewer);
+
+			const bucket = import.meta.env.VITE_BUCKET_URL;
+			const url = `${bucket}/${projectid}/${fileid}/potree/metadata.json`;
+
+			window.Potree.loadPointCloud(url)
+				.then((e: any) => {
+					const pointcloud = e.pointcloud;
+					const material = pointcloud.material;
+					material.activeAttributeName = "point source id";
+					material.minSize = 2;
+					material.pointSizeType = window.Potree.PointSizeType.FIXED;
+					newViewer.scene.addPointCloud(pointcloud);
+					newViewer.setFilterPointSourceIDRange(treeid, treeid);
+					newViewer.fitToScreen();
+				})
+				.catch((error: any) => console.error("ERROR: ", error));
+		}
+	}, [potreeContainerRef.current]);
+
 	return (
-	  <div
-		ref={potreeContainerRef}
-		style={{
-		  backgroundColor: "black",
-		  display: "flex",
-		  flexDirection: "column",
-		  height: "100vh",
-		  width: "100vw",
-		  position: "relative",
-		}}
-		className="potree_container"
-	  >
-		<div id="potree_render_area"></div>
-	  </div>
+		<div
+			ref={potreeContainerRef}
+			style={{
+				backgroundColor: "black",
+				display: "flex",
+				flexDirection: "column",
+				height: "100vh",
+				width: "100vw",
+				position: "relative",
+			}}
+			className="potree_container"
+		>
+			<div id="potree_render_area"></div>
+		</div>
 	);
-  }
-  
+}
