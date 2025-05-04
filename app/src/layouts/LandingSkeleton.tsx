e0aa2f55ec67bbe8156b44adfdd001bc110cf379
rod <rodrigo.eguiluz@udem.edu>
2025-05-04 17:03:57 -0600

    menubar now changes after logging in

--- app/src/layouts/LandingSkeleton.tsx
+++ app/src/layouts/LandingSkeleton.tsx
@@ -1,26 +1,24 @@
 import { LoginHeader } from "@/components/login-header";
-import {SidebarProvider } from "@/components/ui/sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { UserState } from "@/interfaces/redux";
 import Landing from "@/views/landing-info/Landing";
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function LandingSkeleton() {
+
+    const token = useSelector(selectToken);
+    const isLoggedIn = token !== "" && token !== undefined && token !== null;
 
     return (
 
         <>
 
             <div className="[--header-height:calc(theme(spacing.14))]">
+                {isLoggedIn ? <SiteHeader /> : <LoginHeader />}
 
-                    <LoginHeader />
-
-                    <div className="flex flex-1">
-
-
-                        <Landing/>
-
-
-                    </div>
-
+                <div className="flex flex-1">
+                    <Landing/>
+                </div>
             </div>
 
 
@@ -30,3 +28,5 @@ export default function LandingSkeleton() {
     )
 
 }
+
+export const selectToken = (state : {userState : UserState}) => state.userState.token;
