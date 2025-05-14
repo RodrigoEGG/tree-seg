5c54e85f660f3ebb63d19c88d1d6d44a9c9bd81f
Juan Cuevas <juan.m.cuevas@outlook.com>
2025-05-13 22:39:29 -0600

    MS : Update tree

--- microservices/app/api/endpoints/trees.py
+++ microservices/app/api/endpoints/trees.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from app.crud.trees.trees import get_tree_by_id
+from app.crud.trees.trees import get_tree_by_id, update_tree_attributes
 from pymongo.database import Database
 from app.dependencies.mongo_depends import get_mongo
 
@@ -12,3 +12,11 @@ def fetch_tree_by_id( file_id : int, tree_id : int ,client : Database = Depends(
 		return tree
 	except Exception as e:
 		return {}
+	
+@router.post("/{file_id}/{tree_id}", status_code=status.HTTP_200_OK)
+def update_tree_by_id( file_id : int, tree_id : int , data : dict, client : Database = Depends(get_mongo)):
+	try:
+		check = update_tree_attributes(client, file_id, tree_id, data)
+		return {"check" : check}
+	except Exception as e:
+		return {"check" : False}
--- microservices/app/crud/trees/trees.py
+++ microservices/app/crud/trees/trees.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from pymongo.database import Database
 from bson import ObjectId
+from pymongo.errors import PyMongoError
 
 def get_tree_by_id(client : Database, file_id: str, tree_id: str):
     
@@ -31,6 +32,33 @@ def get_tree_by_id(client : Database, file_id: str, tree_id: str):
 			return {}
 	except Exception as e:
 		return {}
+	
+
+def update_tree_attributes(client: Database, file_id: int, tree_id: int, update_data: dict) -> bool:
+    
+    try:
+        db = client['tree-seg']
+        collection = db['metadata']
+
+        set_data = {f"tree_data.$.{key}": value for key, value in update_data.items()}
+
+        result = collection.update_one(
+            {
+                "file_id": file_id,
+                "tree_data.treeid": tree_id
+            },
+            {
+                "$set": set_data
+            }
+        )
+
+        if result.matched_count == 0:
+            return False
+
+        return True
+
+    except PyMongoError as e:
+        return False
 
 
 
