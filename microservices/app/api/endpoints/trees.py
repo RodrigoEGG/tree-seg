305d85a15763a5e52e105247a3e246dd60090d20
Juan Cuevas <juan.m.cuevas@outlook.com>
2025-05-13 22:07:37 -0600

    MS : Retrieve file by id

--- microservices/app/api/endpoints/trees.py
+++ microservices/app/api/endpoints/trees.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from app.crud.trees.trees import get_tree_by_id
 from pymongo.database import Database
 from app.dependencies.mongo_depends import get_mongo
 
@@ -7,6 +8,7 @@ router = APIRouter()
 @router.get("/{file_id}/{tree_id}", status_code=status.HTTP_200_OK)
 def fetch_tree_by_id( file_id : int, tree_id : int ,client : Database = Depends(get_mongo)):
 	try:
-		tree = get_tree(client, file_id, tree_id)
+		tree = get_tree_by_id(client, file_id, tree_id)
+		return tree
 	except Exception as e:
 		return {}
---
+++ microservices/app/crud/trees/trees.py
@@ -0,0 +1,38 @@
+from datetime import timedelta, datetime
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+from pymongo.database import Database
+from bson import ObjectId
+
+def get_tree_by_id(client : Database, file_id: str, tree_id: str):
+    
+	try: 
+
+		db = client['tree-seg']
+		collection = db['metadata']
+
+		result = collection.find_one(
+			{
+				"file_id": file_id,
+				"tree_data.treeid": tree_id
+			},{
+				"file_id": 1,
+				"tree_data": {
+					"$elemMatch": {
+						"treeid": tree_id
+					}
+				}
+			}
+		)
+
+		if result['tree_data'][0]:
+			return result['tree_data'][0]
+		else:
+			return {}
+	except Exception as e:
+		return {}
+
+
+
+
+
