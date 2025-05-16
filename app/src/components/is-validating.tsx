dfdc900dbe5f765ba68fecd65ea7f7e29774c104
Juan Cuevas <juan.m.cuevas@outlook.com>
2025-05-16 17:41:09 -0600

    App : validate tree id limit

---
+++ app/src/components/is-validating.tsx
@@ -0,0 +1,15 @@
+
+export default function IsValidating() {
+	return (
+		<>
+
+			<div className="flex items-center justify-center h-screen">
+				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+					<p className="text-lg font-semibold">Validating data...</p>
+					<p className="text-sm text-gray-500">Please wait a moment.</p>
+				</div>
+			</div>
+		
+		</>
+	)
+}
--- app/src/views/tree/Tree.tsx
+++ app/src/views/tree/Tree.tsx
@@ -1,10 +1,54 @@
-// @ts-nocheck
+//@ts-check
 import { TreeProvider } from "@/context/TreeProvider"
 import Menu from "./components/Menu"
 import Viewer from "./components/Viewer"
+import useSWR from "swr"
+import { fileServices } from "@/services/file-api"
+import { FileMetadata } from "@/interfaces/file-record"
+import { useEffect, useState } from "react"
+import { selectToken } from "@/redux/slices/useSlice"
+import { useSelector } from "react-redux"
+import { useNavigate, useParams } from "react-router-dom"
+import IsValidating from "@/components/is-validating"
 
 
 export default function Tree(){
+
+	const {fileid, treeid} = useParams();
+	const navigate = useNavigate();
+	const token = useSelector(selectToken);
+
+	const [metadata, setMetadata] = useState<FileMetadata>();
+
+	const fetcher = async () => {
+		const data = await fileServices.getMetadataByFile(parseInt(fileid ? fileid : "1"), token);
+		return data;
+	}
+
+	const { data,error } = useSWR(fileid ? `/api/files/metadata/${fileid}` : null, fetcher, { 
+		revalidateOnFocus: false, 
+		revalidateOnReconnect: false 
+	});
+
+	useEffect(()=>{
+		if(data){
+
+			if(data.tree_data.length < parseInt(treeid ? treeid : "-1") || treeid == '0'){
+				navigate("/app/projects");
+			}
+
+		}
+	},[data])
+
+	if (error) return (
+		<div>
+			Failed to load data
+		</div>
+	);
+
+	if (!data) return (
+		<IsValidating/>
+	);
 
 	return (
 
